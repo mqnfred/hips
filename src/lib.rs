0b34cc6acb546b15fe437a72965faeac5f01ee31
Louis Feuvrier <mqnfred@gmail.com>
2020-01-27 15:52:33 -0800

    Fix encryption abstraction

--- src/lib.rs
+++ src/lib.rs
@@ -4,67 +4,41 @@ extern crate magic_crypt;
 use ::std::collections::BTreeMap;
 use ::std::io::Write;
 
+pub struct Database<S: Store, E: Encrypter> {
+    s: S,
+    e: E,
+}
+
+pub type DefaultDatabase = Database<stores::YAML, encrypters::Magic>;
+
+impl Database<stores::YAML, encrypters::Magic> {
+    pub fn new(path: String, password: String) -> Self {
+        Self{ s: stores::YAML::new(path), e: encrypters::Magic::new(password) }
+    }
+}
+
+impl<S: Store, E: Encrypter> Store for Database<S, E> {
+    fn set(&mut self, key: String, value: String) -> Result<(), ::failure::Error> {
+        self.s.set(key, self.e.encrypt(&value)?)
+    }
+
+    fn get(&mut self, key: String) -> Result<String, ::failure::Error> {
+        self.e.decrypt(&self.s.get(key)?)
+    }
+
+    fn all(&mut self) -> Result<BTreeMap<String, String>, ::failure::Error> {
+        let mut map = BTreeMap::<String, String>::new();
+        for (k, v) in self.s.all()? {
+            map.insert(k, self.e.decrypt(&v)?);
+        }
+        Ok(map)
+    }
+}
+
 pub trait Store {
     fn set(&mut self, key: String, value: String) -> Result<(), ::failure::Error>;
     fn get(&mut self, key: String) -> Result<String, ::failure::Error>;
     fn all(&mut self) -> Result<BTreeMap<String, String>, ::failure::Error>;
-}
-
-pub struct YAMLStore<E: Encrypter> {
-    path: String,
-    encrypter: E,
-}
-
-impl<E: Encrypter> Store for YAMLStore<E> {
-    fn set(&mut self, key: String, value: String) -> Result<(), ::failure::Error> {
-        let mut map = self.read()?;
-        map.insert(key, value);
-        self.write(map)
-    }
-
-    fn get(&mut self, key: String) -> Result<String, ::failure::Error> {
-        self.read()?.get(&key).map(|f| {
-            Ok(f.to_owned())
-        }).unwrap_or_else(|| Err(::failure::err_msg("key not found")))
-    }
-
-    fn all(&mut self) -> Result<BTreeMap<String, String>, ::failure::Error> {
-        self.read()
-    }
-}
-
-impl<E: Encrypter> YAMLStore<E> {
-    pub fn new(path: String, password: String) -> Self {
-        Self { path, encrypter: E::new(password) }
-    }
-
-    fn read(&mut self) -> Result<BTreeMap<String, String>, ::failure::Error> {
-        let mut encrypted_map: BTreeMap<String, String> = ::serde_yaml::from_str(
-            &match ::std::fs::read_to_string(&self.path) {
-                Err(err) if err.kind() == ::std::io::ErrorKind::NotFound => Ok("{}".to_string()),
-                Err(err) => Err(err),
-                Ok(val) => Ok(val),
-            }?,
-        )?;
-
-        let mut map = BTreeMap::<String, String>::new();
-        for (k, v) in encrypted_map.into_iter() {
-            map.insert(k, self.encrypter.decrypt(&v)?);
-        }
-        Ok(map)
-    }
-
-    fn write(&mut self, mut map: BTreeMap<String, String>) -> Result<(), ::failure::Error> {
-        let mut encrypted_map = BTreeMap::<String, String>::new();
-        for (k, v) in map.into_iter() {
-            encrypted_map.insert(k, self.encrypter.encrypt(&v)?);
-        }
-
-        let yaml = serde_yaml::to_string(&encrypted_map)?;
-        let mut file = ::std::fs::OpenOptions::new().write(true).create(true).truncate(true).open(&self.path)?;
-        file.write_all(yaml.as_bytes())?;
-        Ok(())
-    }
 }
 
 pub trait Encrypter {
@@ -73,20 +47,69 @@ pub trait Encrypter {
     fn decrypt(&mut self, value: &str) -> Result<String, ::failure::Error>;
 }
 
-pub struct MagicEncrypter(::magic_crypt::MagicCrypt);
+mod stores {
+    use super::*;
 
-impl Encrypter for MagicEncrypter {
-    fn new(key: String) -> Self {
-        Self(::magic_crypt::new_magic_crypt!(&key, 256))
+    pub struct YAML {
+        path: String,
     }
 
-    fn encrypt(&mut self, value: &str) -> Result<String, ::failure::Error> {
-        Ok(self.0.encrypt_str_to_base64(value))
+    impl Store for YAML {
+        fn set(&mut self, key: String, value: String) -> Result<(), ::failure::Error> {
+            let mut map = self.read()?;
+            map.insert(key, value);
+            self.write(map)
+        }
+
+        fn get(&mut self, key: String) -> Result<String, ::failure::Error> {
+            self.read()?.get(&key).map(|f| {
+                Ok(f.to_owned())
+            }).unwrap_or_else(|| Err(::failure::err_msg("key not found")))
+        }
+
+        fn all(&mut self) -> Result<BTreeMap<String, String>, ::failure::Error> {
+            self.read()
+        }
     }
 
-    fn decrypt(&mut self, value: &str) -> Result<String, ::failure::Error> {
-        Ok(self.0.decrypt_base64_to_string(value).map_err(|err| {
-            ::failure::err_msg("failed_to_decrypt")
-        })?)
+    impl YAML {
+        pub fn new(path: String) -> Self {
+            Self { path }
+        }
+
+        fn read(&mut self) -> Result<BTreeMap<String, String>, ::failure::Error> {
+            Ok(::serde_yaml::from_str(&match ::std::fs::read_to_string(&self.path) {
+                Err(err) if err.kind() == ::std::io::ErrorKind::NotFound => Ok("{}".to_string()),
+                Err(err) => Err(err),
+                Ok(val) => Ok(val),
+            }?)?)
+        }
+
+        fn write(&mut self, map: BTreeMap<String, String>) -> Result<(), ::failure::Error> {
+            let mut f = ::std::fs::OpenOptions::new().write(true).create(true).truncate(true).open(&self.path)?;
+            Ok(f.write_all(::serde_yaml::to_string(&map)?.as_bytes())?)
+        }
+    }
+}
+
+mod encrypters {
+    use super::*;
+
+    pub struct Magic(::magic_crypt::MagicCrypt);
+
+    impl Encrypter for Magic {
+        fn new(key: String) -> Self {
+            Self(::magic_crypt::new_magic_crypt!(&key, 256))
+        }
+
+        fn encrypt(&mut self, value: &str) -> Result<String, ::failure::Error> {
+            Ok(self.0.encrypt_str_to_base64(value))
+        }
+
+        fn decrypt(&mut self, value: &str) -> Result<String, ::failure::Error> {
+            Ok(self.0.decrypt_base64_to_string(value).map_err(|err| {
+                ::failure::err_msg("failed_to_decrypt")
+            })?)
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate clap;
 
 fn main() -> Result<(), ::failure::Error> {
     let opts = Options::parse();
-
     match opts.subcmd {
         Command::Env(env) => env.run(opts.store, opts.password),
         Command::Set(set) => set.run(opts.store, opts.password),
@@ -27,13 +26,61 @@ struct Options {
 #[derive(Clap, Debug)]
 enum Command {
     #[clap(name = "env", about = "Output a shell script which loads all keys as environment")]
-    Env(env::Env),
+    Env(commands::Env),
     #[clap(name = "get", about = "Get the value for a given key")]
-    Get(get::Get),
+    Get(commands::Get),
     #[clap(name = "set", about = "Set a key to a given value")]
-    Set(set::Set),
+    Set(commands::Set),
 }
 
-mod env;
-mod get;
-mod set;
+
+mod commands {
+    use ::hips::Store;
+    use ::std::io::Write;
+
+    #[derive(Clap, Debug)]
+    pub struct Env {
+        #[clap(short = "i", long = "interpreter")]
+        interpreter: Option<String>,
+    }
+    impl Env {
+        pub fn run(self, store: String, pw: String) -> Result<(), ::failure::Error> {
+            let mut db = ::hips::DefaultDatabase::new(store, pw);
+            let assignments = db.all()?.into_iter().map(|(k, v)| {
+                format!("export {} = '{}';", k.to_uppercase(), v)
+            }).collect::<Vec<String>>();
+
+            if let Some(interpreter) = self.interpreter {
+                writeln!(::std::io::stdout(), "#!{}\n", interpreter);
+            }
+            Ok(writeln!(::std::io::stdout(), "{}", assignments.join("\n"))?)
+        }
+    }
+
+    #[derive(Clap, Debug)]
+    pub struct Get {
+        #[clap(name = "key")]
+        key: String,
+    }
+    impl Get {
+        pub fn run(self, store: String, pw: String) -> Result<(), ::failure::Error> {
+            let mut db = ::hips::DefaultDatabase::new(store, pw);
+            writeln!(::std::io::stdout(), "{}", db.get(self.key)?);
+            Ok(())
+        }
+    }
+
+    #[derive(Clap, Debug)]
+    pub struct Set {
+        #[clap(name = "key")]
+        key: String,
+        #[clap(name = "value")]
+        value: String,
+    }
+    impl Set {
+        pub fn run(self, store: String, pw: String) -> Result<(), ::failure::Error> {
+            let mut db = ::hips::DefaultDatabase::new(store, pw);
+            db.set(self.key, self.value)
+        }
+    }
+}
