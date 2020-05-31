afbd365a3ae429b158ae08d89bb06b3d0f4ff16a
Louis Feuvrier <mqnfred@gmail.com>
2020-05-30 18:13:55 -0700

    Improve error handling of env, Split commands mod

---
+++ src/commands.rs
@@ -0,0 +1,58 @@
+use ::modularcli::prelude::*;
+use ::std::io::Write;
+
+commands! {
+    #[clap(about = "Store provided secret under the provided name")]
+    Store(self, db: &mut hips::Database) -> Result<()> {
+        db.store(hips::Secret{name: self.name, secret: self.secret})
+    } struct {
+        #[clap(about = "The name to store/hide the secret under")]
+        name: String,
+        #[clap(about = "The secret to store and hide")]
+        secret: String,
+    },
+
+    #[clap(about = "Retrieve secret under the provided name")]
+    Load(self, db: &mut hips::Database) -> Result<()> {
+        writeln!(::std::io::stdout(), "{}", db.load(self.name)?.secret)?;
+        Ok(())
+    } struct {
+        #[clap(about = "The name to retrieve the secrets for")]
+        name: String,
+    },
+
+    #[clap(alias = "rm", about = "Remove the secret under the provided name")]
+    Remove(self, db: &mut hips::Database) -> Result<()> {
+        db.remove(self.name)
+    } struct {
+        #[clap(about = "The name to retrieve the secrets for")]
+        name: String,
+    },
+
+    #[clap(alias = "rot", about = "Re-encrypt the whole database using a new password")]
+    Rotate(self, db: &mut hips::Database) -> Result<()> {
+        let db_path = ::std::env::var("HIPS_DATABASE")?.into();
+        let mut new_db = hips::Database::new(db_path, self.new_password)?;
+        for secret in db.list()? {
+            new_db.store(secret)?;
+        }
+        Ok(())
+    } struct {
+        #[clap(name = "new-password", about = "The password to re-encrypt the database with")]
+        new_password: String,
+    },
+
+    #[clap(alias = "tmp", about = "Print one or multiple secrets according to a template")]
+    Template(self, db: &mut hips::Database) -> Result<()> {
+        let template = match ::std::fs::read_to_string(&self.template) {
+            Err(err) if err.kind() == ::std::io::ErrorKind::NotFound => Ok(self.template),
+            Err(err) => Err(err),
+            Ok(val) => Ok(val),
+        }?;
+        writeln!(::std::io::stdout(), "{}", db.template(template)?)?;
+        Ok(())
+    } struct {
+        #[clap(about = "Template or path to file containing the template")]
+        template: String,
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate modularcli;
 use modularcli::prelude::*;
 use ::std::io::Write;
 
-fn main() -> Result<(), Error> {
+fn main() -> Result<()> {
     if let Err(err) = run() {
         writeln!(::std::io::stderr(), "error: {:#}", err)?;
         ::std::process::exit(1);
@@ -14,14 +14,24 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn run() -> Result<(), Error> {
-    let db_path = ::std::env::var("HIPS_DATABASE")?.into();
-    let password = ::std::env::var("HIPS_PASSWORD")?;
+fn run() -> Result<()> {
+    let db_path = unwrap_env_var("HIPS_DATABASE")?.into();
+    let password = unwrap_env_var("HIPS_PASSWORD")?;
     Hips::parse().run(&mut ::hips::Database::new(db_path, password)?)
 }
 
 dispatchers! {
-    #[clap(author = "Louis Feuvrier, mqnfred@gmail.com")]
+    #[clap(
+        name = "hips",
+        about = "Manage secrets alongside your code",
+        author = "Louis Feuvrier <mqnfred@gmail.com>",
+        version = "0.1.0",
+        after_help = "\
+            ENVIRONMENT:\n    \
+            HIPS_DATABASE\tfile/folder containing the secrets (mandatory)\n    \
+            HIPS_PASSWORD\tpassword that will unlock the database (mandatory)\
+        ",
+    )]
     Hips(self, _: &mut hips::Database) -> Result<()> [
         Store: commands::Store,
         Load: commands::Load,
@@ -30,64 +40,17 @@ dispatchers! {
         Template: commands::Template,
     ],
 }
+mod commands;
 
-mod commands {
-    use ::modularcli::prelude::*;
-    use ::std::io::Write;
+fn unwrap_env_var(name: &str) -> Result<String> {
+    let var = ::std::env::var(name).map_err(|err| {
+        Error::msg(format!("{}: {}", err, name))
+    });
 
-    commands! {
-        #[clap(about = "Store provided secret under the provided name")]
-        Store(self, db: &mut hips::Database) -> Result<()> {
-            db.store(hips::Secret{name: self.name, secret: self.secret})
-        } struct {
-            #[clap(about = "The name to store/hide the secret under")]
-            name: String,
-            #[clap(about = "The secret to store and hide")]
-            secret: String,
-        },
-
-        #[clap(about = "Retrieve secret under the provided name")]
-        Load(self, db: &mut hips::Database) -> Result<()> {
-            writeln!(::std::io::stdout(), "{}", db.load(self.name)?.secret)?;
-            Ok(())
-        } struct {
-            #[clap(about = "The name to retrieve the secrets for")]
-            name: String,
-        },
-
-        #[clap(alias = "rm", about = "Remove the secret under the provided name")]
-        Remove(self, db: &mut hips::Database) -> Result<()> {
-            db.remove(self.name)
-        } struct {
-            #[clap(about = "The name to retrieve the secrets for")]
-            name: String,
-        },
-
-        #[clap(alias = "rot", about = "Re-encrypt the whole database using a new password")]
-        Rotate(self, db: &mut hips::Database) -> Result<()> {
-            let db_path = ::std::env::var("HIPS_DATABASE")?.into();
-            let mut new_db = hips::Database::new(db_path, self.new_password)?;
-            for secret in db.list()? {
-                new_db.store(secret)?;
-            }
-            Ok(())
-        } struct {
-            #[clap(name = "new-password", about = "The password to re-encrypt the database with")]
-            new_password: String,
-        },
-
-        #[clap(alias = "tmp", about = "Print one or multiple secrets according to a template")]
-        Template(self, db: &mut hips::Database) -> Result<()> {
-            let template = match ::std::fs::read_to_string(&self.template) {
-                Err(err) if err.kind() == ::std::io::ErrorKind::NotFound => Ok(self.template),
-                Err(err) => Err(err),
-                Ok(val) => Ok(val),
-            }?;
-            writeln!(::std::io::stdout(), "{}", db.template(template)?)?;
-            Ok(())
-        } struct {
-            #[clap(about = "Template or path to file containing the template")]
-            template: String,
-        },
+    if var.is_err() {
+        eprintln!("hips expects both a database file/folder and its");
+        eprintln!("password to be provided as environment variables");
     }
+
+    var
 }
