7a97e959a87f3e522a305134437d98766a25269b
Louis Feuvrier <mqnfred@gmail.com>
2020-06-01 13:45:07 -0700

    (cargo-release) start next development iteration 0.3.1-alpha.0

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ dispatchers! {
         name = "hips",
         about = "Manage secrets alongside your code",
         author = "Louis Feuvrier <mqnfred@gmail.com>",
-        version = "0.3.0",
+        version = "0.3.0-alpha.0",
         after_help = "\
             ENVIRONMENT:\n    \
             HIPS_DATABASE\tfile/folder containing the secrets (mandatory)\n    \
